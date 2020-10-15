6efd9e6dd4cfbaad3e30c45566fbae3adc466f30
Oh Dany <ohdany@Oh-Dany.local>
2020-10-15 13:11:20 -0500

    Determinig Error Type with subclassing for custom errors

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/reques-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,15 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Somthing went wrong', err);
+  if (err instanceof RequestValidationError) {
+    console.log('Handling this error as request validation error');
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log('Handling this error as a db connecton error');
+  }
 
   res.status(400).send({
-    message: 'Somthing went wrong',
+    message: err.message,
   });
 };
