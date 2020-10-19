9b743c995d765ff93641d666967ed1d873684740
Oh Dany <ohdany@Oh-Dany.local>
2020-10-19 00:44:07 -0500

    Sign In logic User

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,7 +1,11 @@
 import expres, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
+import { Password } from '../services/password';
+import { User } from '../models/user';
 import { ValidateRequest } from '../middlewares/validate-request';
+import { BaddRequestError } from '../errors/bad-request-error';
 
 const router = expres.Router();
 
@@ -15,7 +19,38 @@ router.post(
       .withMessage('You must supply a password'),
   ],
   ValidateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BaddRequestError('Invalid credentials');
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BaddRequestError('Invalid credentials');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
